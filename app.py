caf92f8c446e8b8af388a3ac9d9fdcd69c851149
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2022-02-26 21:55:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-# ---------------------------------------------------------------------------- #
-#          Aplicación para leer laboratorios y dar una interpretación          #
-# ---------------------------------------------------------------------------- #
 import streamlit as st
 st.title("LAB app")
 st.image("WAPP.png")
