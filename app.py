bdf579df04b10d5221fdea45c9498f9c6038368d
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2022-03-05 16:54:01 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
+from email.utils import collapse_rfc2231_value
+from operator import truth
+import iniconfig
+from matplotlib import container
 import streamlit as st
-st.title("LAB app")
 st.image("WAPP.png")
-Lista=[]
-a=st.number_input("Ola")
-Lista.append(a)
+st.subheader("Labapp")
+st.markdown("Elige el laboratorio que quieres analizar")
+sol1,sol2,sol3=st.columns(3)
+seleccion=st.selectbox("Laboratorios",['Biometría hemática','Química sanguinea','Hormonas tiroideas'])
+inicio=st.button("Iniciamos")
+
+if seleccion=="Biometría hemática" and inicio==True:
+    st.cache(persist=True)
+    c=st.container()
+    Leu=c.slider("Leucocitos cel/mm3",1,700000)
+
+
