93cdd59e6649c59ffa8033c1b61e63ef590948ba
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2022-03-06 21:08:52 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ st.image("WAPP.png")
 sol1,sol2,sol3=st.columns(3)
 seleccion=st.selectbox("Laboratorios, elige el laboratorio que quieres analizar",['Selecciona','Glucosa','Urea','Creatinina','Trigliceridos','Colesterol total'])
 if seleccion=="Selecciona":
-    st.image('Lab.png',None,250)
+    st.image('lab.png',None,250)
 
 
