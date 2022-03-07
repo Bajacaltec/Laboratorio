9973b9960cd40fa0a08c1b3402468fb48267634d
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2022-03-06 21:10:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,7 @@ sol1,sol2,sol3=st.columns(3)
 seleccion=st.selectbox("Laboratorios, elige el laboratorio que quieres analizar",['Selecciona','Glucosa','Urea','Creatinina','Trigliceridos','Colesterol total'])
 if seleccion=="Selecciona":
     st.image('lab.png',None,250)
+    st.balloons
+    st.subheader("Hola baby")
 
 
