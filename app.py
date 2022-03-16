002060b0512baf9eae561bca21d20edb6c770859
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2022-03-16 15:01:05 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 import streamlit as st
 st.image("WAPP.png")
-seleccion=st.selectbox("Laboratorios, elige el laboratorio que quieres analizar",['Selecciona','Glucosa','Urea','Creatinina','Trigliceridos','Colesterol total'])
 col1,col2,col3=st.columns(3)
-tol1,tol2,tol3,tol4,tol5=st.columns(5)
+with col2:
+    st.subheader('Elige el laboratorio a analizar')
+    seleccion=st.selectbox("",['Selecciona','Glucosa','Creatinina','Trigliceridos'])
 if seleccion=="Selecciona":
     with col2:
         st.image('lab.png',None,250)
 elif seleccion=="Glucosa":
     glu=st.number_input("Glucosa (mg/dl)",0,400,0,1)
     if glu<=70 and glu >0:
-        st.warning("tu azucar puede estar bajo los niveles normales, se guiere consumir algún alimento dulce como un jugo")
+        tol1,tol2,tol3,tol4,tol5=st.columns(5)
         with tol1:
+            st.warning("tu azucar puede estar bajo los niveles normales, se guiere consumir algún alimento dulce como un jugo")
+        with tol2:
             st.image("jugonaranja.jpg")
-        st.subheader("Si utilizas insulina es probable que requieras una disminución de tus aportes de insulina")
-
+        with tol3:
+            st.warning("Si utilizas insulina es probable que requieras una disminución de tus aportes de insulina")
+        with tol4:
+            st.error("Recuerda que si tu azucar está baja y el nivel de consciencia esta disminuido, no deben ingerirse alimentos vía oral, por el riesgo de broncoaspiración, contacta a los servicios de salud de inmediato")
 
