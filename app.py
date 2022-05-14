9e8be2eaf4edb891e484e4dac98a067415415d1e
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2022-05-14 17:25:29 -0500

    a

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from typing import Any
 import streamlit as st
 st.image("WAPP.png")
 col1,col2,col3=st.columns(3)
@@ -9,6 +10,7 @@ if seleccion=="Selecciona":
         st.image('lab.png',None,250)
 elif seleccion=="Glucosa":
     glu=st.number_input("Glucosa (mg/dl)",0,400,0,1)
+    embarazo=st.checkbox("¿Estas embarazada?")
     if glu<=70 and glu >0:
         tol1,tol2,tol3,tol4,tol5=st.columns(5)
         with tol1:
@@ -19,4 +21,6 @@ elif seleccion=="Glucosa":
             st.warning("Si utilizas insulina es probable que requieras una disminución de tus aportes de insulina")
         with tol4:
             st.error("Recuerda que si tu azucar está baja y el nivel de consciencia esta disminuido, no deben ingerirse alimentos vía oral, por el riesgo de broncoaspiración, contacta a los servicios de salud de inmediato")
-
+    elif glu>=120 and embarazo==True:
+        st.success("Estas embarazada")
+        st.balloons()
